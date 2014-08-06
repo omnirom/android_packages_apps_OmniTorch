4d9096e45960d166154b7566347df6883ce88246
maxwen <max.weninger@gmail.com>
2014-08-06 22:22:05 +0200

    OmniTorch: added activity start broadcast
    Add new broadcast that starts the torch with activity
    and not only the service. This is used e.g. for
    starting torch from offscreeen gesture
    
    Added keep screen on settings and fix format of all
    sources files to be AOSP conform
    
    Change-Id: I062f322cc3b7c3e2134b6d82f317d1097311c4a2

--- src/org/omnirom/torch/TorchService.java
+++ src/org/omnirom/torch/TorchService.java
@@ -27,196 +27,216 @@ import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.UserHandle;
+import android.os.Bundle;
 import android.util.Log;
 
 public class TorchService extends Service {
 
-        private static final String TAG = "TorchRoot";
+    private static final String TAG = "TorchService";
 
-        private TimerTask mTorchTask;
-        private Timer mTorchTimer;
-        private WrapperTask mStrobeTask;
-        private Timer mStrobeTimer;
-        private Timer mSosTimer;
-        private WrapperTask mSosTask;
-        private Runnable mSosOnRunnable;
-        private Runnable mSosOffRunnable;
-        private int mSosCount;
-        private NotificationManager mNotificationManager;
-        private Notification mNotification;
-        private Notification.Builder mNotificationBuilder;
-        private int mStrobePeriod;
-        private boolean mBright;
-        private boolean mSos;
-        private boolean mStrobe;
-        private Runnable mStrobeRunnable;
-        private Context mContext;
+    private TimerTask mTorchTask;
+    private Timer mTorchTimer;
+    private WrapperTask mStrobeTask;
+    private Timer mStrobeTimer;
+    private Timer mSosTimer;
+    private WrapperTask mSosTask;
+    private Runnable mSosOnRunnable;
+    private Runnable mSosOffRunnable;
+    private int mSosCount;
+    private NotificationManager mNotificationManager;
+    private Notification.Builder mNotificationBuilder;
+    private int mStrobePeriod;
+    private boolean mBright;
+    private boolean mSos;
+    private boolean mStrobe;
+    private Runnable mStrobeRunnable;
+    private Context mContext;
 
-        public void onCreate() {
-                Log.d(TAG, "onCreate");
-                String ns = Context.NOTIFICATION_SERVICE;
-                mNotificationManager = (NotificationManager) getSystemService(ns);
-                mContext = getApplicationContext();
+    public void onCreate() {
+        Log.d(TAG, "onCreate");
+        String ns = Context.NOTIFICATION_SERVICE;
+        mNotificationManager = (NotificationManager) getSystemService(ns);
+        mContext = getApplicationContext();
 
-                mTorchTask = new TimerTask() {
-                        public void run() {
-                                FlashDevice.getInstance(mContext).setFlashMode(FlashDevice.ON, mBright);
-                        }
-                };
-                mTorchTimer = new Timer();
+        mTorchTask = new TimerTask() {
+            public void run() {
+                FlashDevice.getInstance(mContext).setFlashMode(FlashDevice.ON,
+                        mBright);
+            }
+        };
+        mTorchTimer = new Timer();
 
-                mStrobeRunnable = new Runnable() {
-                        private int mCounter = 4;
+        mStrobeRunnable = new Runnable() {
+            private int mCounter = 4;
 
-                        public void run() {
-                                int flashMode = FlashDevice.ON;
-                                if (FlashDevice.getInstance(mContext).getFlashMode() == FlashDevice.STROBE) {
-                                        if (mCounter-- < 1) {
-                                                FlashDevice.getInstance(mContext).setFlashMode(flashMode, mBright);
-                                        }
-                                } else {
-                                        FlashDevice.getInstance(mContext).setFlashMode(FlashDevice.STROBE, mBright);
-                                        mCounter = 4;
-                                }
-                        }
-
-                };
-                mStrobeTask = new WrapperTask(mStrobeRunnable);
-                mStrobeTimer = new Timer();
-
-                mSosOnRunnable = new Runnable() {                        
-                        public void run() {
-                                FlashDevice.getInstance(mContext).setFlashMode(FlashDevice.ON, mBright);
-                                mSosTask = new WrapperTask(mSosOffRunnable);
-                                int schedTime = 0;
-                                switch(mSosCount){
-                                        case 0:
-                                        case 1:
-                                        case 2:
-                                        case 6:
-                                        case 7:
-                                        case 8:
-                                                schedTime = 200;
-                                                break;
-                                        case 3:
-                                        case 4:
-                                        case 5:
-                                                schedTime = 600;
-                                                break;
-                                        default:
-                                                return;
-                                }
-                if (mSosTimer != null){
-                                    mSosTimer.schedule(mSosTask, schedTime);
-                }
-                        }
-                };
-
-                mSosOffRunnable = new Runnable() {                        
-                        public void run() {
-                                FlashDevice.getInstance(mContext).setFlashMode(FlashDevice.OFF, mBright);
-                                mSosTask = new WrapperTask(mSosOnRunnable);
-                                mSosCount++;
-                                if (mSosCount == 9){
-                                        mSosCount = 0;
-                                }
-                if (mSosTimer != null){
-                                    mSosTimer.schedule(mSosTask, mSosCount == 0 ? 2000:200);
-                }
-                        }
-                };
-
-                mSosTask = new WrapperTask(mSosOnRunnable);
-                mSosTimer = new Timer();
-        }
-
-        public int onStartCommand(Intent intent, int flags, int startId) {
-
-                Log.d(TAG, "onStartCommand");
-                if (intent == null) {
-                        stopSelf();
-                        return START_NOT_STICKY;
-                }
-                mBright = intent.getBooleanExtra("bright", false);
-                mStrobe = intent.getBooleanExtra("strobe", false);
-                mSos = intent.getBooleanExtra("sos", false);
-
-                int strobePeriod = intent.getIntExtra("period", 5);
-                if (strobePeriod == 0){
-                        strobePeriod = 1;
-                }
-                mStrobePeriod = (666 / strobePeriod) / 4;
-
-                if (mSos){
-                        mBright = true;
-                        mStrobe = false;
-                }
-
-                Log.d(TAG, "onStartCommand mBright = " + mBright + " mStrobe = " +
-                                        mStrobe + " mStrobePeriod = " + mStrobePeriod + " mSos = " + mSos);
-
-                if (mSos){
-                        mSosTimer.schedule(mSosTask, 0);                                
-                } else if (mStrobe) {
-                        mStrobeTimer.schedule(mStrobeTask, 0, mStrobePeriod);
+            public void run() {
+                int flashMode = FlashDevice.ON;
+                if (FlashDevice.getInstance(mContext).getFlashMode() == FlashDevice.STROBE) {
+                    if (mCounter-- < 1) {
+                        FlashDevice.getInstance(mContext).setFlashMode(
+                                flashMode, mBright);
+                    }
                 } else {
-                        mTorchTimer.schedule(mTorchTask, 0, 100);
+                    FlashDevice.getInstance(mContext).setFlashMode(
+                            FlashDevice.STROBE, mBright);
+                    mCounter = 4;
                 }
-                mNotificationBuilder = new Notification.Builder(this);
-                mNotificationBuilder.setSmallIcon(R.drawable.ic_torch_on);
-                mNotificationBuilder.setTicker(getString(R.string.not_torch_title));
-                mNotificationBuilder.setContentTitle(getString(R.string.not_torch_title));
-                mNotificationBuilder.setContentIntent(PendingIntent.getActivity(this, 0, new Intent(this,
-                                MainActivity.class), 0));
-                mNotificationBuilder.setAutoCancel(false);
-                mNotificationBuilder.setOngoing(true);
+            }
 
-                PendingIntent turnOff = PendingIntent.getBroadcast(this, 0,
-                                new Intent(TorchSwitch.TOGGLE_FLASHLIGHT), 0);
-                mNotificationBuilder.addAction(R.drawable.ic_torch_off,
-                                getString(R.string.not_torch_toggle), turnOff);
+        };
+        mStrobeTask = new WrapperTask(mStrobeRunnable);
+        mStrobeTimer = new Timer();
 
-                mNotification = mNotificationBuilder.getNotification();
-                mNotificationManager.notify(getString(R.string.app_name).hashCode(), mNotification);
-
-                startForeground(getString(R.string.app_name).hashCode(), mNotification);
-                updateState(true);
-                return START_STICKY;
-        }
-
-        public void onDestroy() {
-                mNotificationManager.cancelAll();
-                stopForeground(true);
-                mTorchTimer.cancel();
-                mTorchTimer = null;
-                mStrobeTimer.cancel();
-                mStrobeTimer = null;
-                mSosTimer.cancel();
-                mSosTimer = null;
-                FlashDevice.getInstance(mContext).setFlashMode(FlashDevice.OFF, mBright);
-                updateState(false);
-        }
-
-        private void updateState(boolean on) {
-                Intent intent = new Intent(TorchSwitch.TORCH_STATE_CHANGED);
-                intent.putExtra("state", on ? 1 : 0);
-                sendStickyBroadcast(intent);
-        }
-
-        public class WrapperTask extends TimerTask {
-                private final Runnable mTarget;
-
-                public WrapperTask(Runnable target) {
-                        mTarget = target;
+        mSosOnRunnable = new Runnable() {
+            public void run() {
+                FlashDevice.getInstance(mContext).setFlashMode(FlashDevice.ON,
+                        mBright);
+                mSosTask = new WrapperTask(mSosOffRunnable);
+                int schedTime = 0;
+                switch (mSosCount) {
+                case 0:
+                case 1:
+                case 2:
+                case 6:
+                case 7:
+                case 8:
+                    schedTime = 200;
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    schedTime = 600;
+                    break;
+                default:
+                    return;
                 }
-
-                public void run() {
-                        mTarget.run();
+                if (mSosTimer != null) {
+                    mSosTimer.schedule(mSosTask, schedTime);
                 }
+            }
+        };
+
+        mSosOffRunnable = new Runnable() {
+            public void run() {
+                FlashDevice.getInstance(mContext).setFlashMode(FlashDevice.OFF,
+                        mBright);
+                mSosTask = new WrapperTask(mSosOnRunnable);
+                mSosCount++;
+                if (mSosCount == 9) {
+                    mSosCount = 0;
+                }
+                if (mSosTimer != null) {
+                    mSosTimer.schedule(mSosTask, mSosCount == 0 ? 2000 : 200);
+                }
+            }
+        };
+
+        mSosTask = new WrapperTask(mSosOnRunnable);
+        mSosTimer = new Timer();
+    }
+
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        Log.d(TAG, "onStartCommand");
+        if (intent == null) {
+            stopSelf();
+            return START_NOT_STICKY;
+        }
+        boolean startActivity = intent.getBooleanExtra("activity", false);
+        mBright = intent.getBooleanExtra("bright", false);
+        mStrobe = intent.getBooleanExtra("strobe", false);
+        mSos = intent.getBooleanExtra("sos", false);
+
+        int strobePeriod = intent.getIntExtra("period", 5);
+        if (strobePeriod == 0) {
+            strobePeriod = 1;
+        }
+        mStrobePeriod = (666 / strobePeriod) / 4;
+
+        if (mSos) {
+            mBright = true;
+            mStrobe = false;
         }
 
-        @Override
-        public IBinder onBind(Intent intent) {
-                return null;
+        Log.d(TAG, "onStartCommand mBright = " + mBright + " mStrobe = "
+                + mStrobe + " mStrobePeriod = " + mStrobePeriod + " mSos = "
+                + mSos + " startActivity = " + startActivity);
+
+        if (mSos) {
+            mSosTimer.schedule(mSosTask, 0);
+        } else if (mStrobe) {
+            mStrobeTimer.schedule(mStrobeTask, 0, mStrobePeriod);
+        } else {
+            mTorchTimer.schedule(mTorchTask, 0, 100);
         }
+        mNotificationBuilder = new Notification.Builder(this);
+        mNotificationBuilder.setSmallIcon(R.drawable.ic_torch_on);
+        mNotificationBuilder.setTicker(getString(R.string.not_torch_title));
+        mNotificationBuilder
+                .setContentTitle(getString(R.string.not_torch_title));
+
+        Intent fullscreenIntent = new Intent(this, MainActivity.class);
+        fullscreenIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        if (startActivity) {
+            mNotificationBuilder.setFullScreenIntent(PendingIntent.getActivity(
+                    this, 0, fullscreenIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT), true);
+        }
+
+        mNotificationBuilder.setContentIntent(PendingIntent.getActivity(this,
+                0, fullscreenIntent, 0));
+        mNotificationBuilder.setAutoCancel(false);
+        mNotificationBuilder.setOngoing(true);
+
+        PendingIntent turnOff = PendingIntent.getBroadcast(this, 0, new Intent(
+                TorchSwitch.TOGGLE_FLASHLIGHT), 0);
+        mNotificationBuilder.addAction(R.drawable.ic_torch_off,
+                getString(R.string.not_torch_toggle), turnOff);
+
+        Notification notification = mNotificationBuilder.build();
+        mNotificationManager.notify(getString(R.string.app_name).hashCode(),
+                notification);
+
+        startForeground(getString(R.string.app_name).hashCode(), notification);
+        updateState(true);
+        return START_STICKY;
+    }
+
+    public void onDestroy() {
+        mNotificationManager.cancelAll();
+        stopForeground(true);
+        mTorchTimer.cancel();
+        mTorchTimer = null;
+        mStrobeTimer.cancel();
+        mStrobeTimer = null;
+        mSosTimer.cancel();
+        mSosTimer = null;
+        FlashDevice.getInstance(mContext)
+                .setFlashMode(FlashDevice.OFF, mBright);
+        updateState(false);
+    }
+
+    private void updateState(boolean on) {
+        Intent intent = new Intent(TorchSwitch.TORCH_STATE_CHANGED);
+        intent.putExtra("state", on ? 1 : 0);
+        sendBroadcastAsUser(intent, UserHandle.CURRENT);
+    }
+
+    public class WrapperTask extends TimerTask {
+        private final Runnable mTarget;
+
+        public WrapperTask(Runnable target) {
+            mTarget = target;
+        }
+
+        public void run() {
+            mTarget.run();
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
--- src/org/omnirom/torch/TorchSwitch.java
+++ src/org/omnirom/torch/TorchSwitch.java
@@ -28,56 +28,84 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.util.Log;
+import android.os.UserHandle;
 
 public class TorchSwitch extends BroadcastReceiver {
+    private static final String TAG = "TorchSwitch";
+    public static final String TOGGLE_FLASHLIGHT = "org.omnirom.torch.TOGGLE_FLASHLIGHT";
+    public static final String START_FLASHLIGHT = "org.omnirom.torch.START_FLASHLIGHT";
+    public static final String TORCH_STATE_CHANGED = "org.omnirom.torch.TORCH_STATE_CHANGED";
 
-        public static final String TOGGLE_FLASHLIGHT = "org.omnirom.torch.TOGGLE_FLASHLIGHT";
-        public static final String TORCH_STATE_CHANGED = "org.omnirom.torch.TORCH_STATE_CHANGED";
+    private SharedPreferences mPrefs;
 
-        private SharedPreferences mPrefs;
+    @Override
+    public void onReceive(Context context, Intent receivingIntent) {
+        mPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        if (receivingIntent.getAction().equals(TOGGLE_FLASHLIGHT)) {
+            // bright setting can come from intent or from prefs depending on
+            // on what send the broadcast
+            boolean bright = receivingIntent.getBooleanExtra("bright",
+                    mPrefs.getBoolean(SettingsActivity.KEY_BRIGHT, false));
+            boolean strobe = receivingIntent.getBooleanExtra("strobe",
+                    mPrefs.getBoolean(SettingsActivity.KEY_STROBE, false));
+            boolean sos = receivingIntent.getBooleanExtra("sos",
+                    mPrefs.getBoolean(SettingsActivity.KEY_SOS, false));
+            int period = receivingIntent.getIntExtra("period",
+                    mPrefs.getInt(SettingsActivity.KEY_STROBE_FREQ, 5));
 
-        @Override
-        public void onReceive(Context context, Intent receivingIntent) {
-                mPrefs = PreferenceManager.getDefaultSharedPreferences(context);
-                if (receivingIntent.getAction().equals(TOGGLE_FLASHLIGHT)) {
-                        // bright setting can come from intent or from prefs depending on
-                        // on what send the broadcast
-                        boolean bright = receivingIntent.getBooleanExtra("bright",
-                                mPrefs.getBoolean(SettingsActivity.KEY_BRIGHT, false));
-                        boolean strobe = receivingIntent.getBooleanExtra("strobe",
-                                mPrefs.getBoolean(SettingsActivity.KEY_STROBE, false));
-                        boolean sos = receivingIntent.getBooleanExtra("sos",
-                                mPrefs.getBoolean(SettingsActivity.KEY_SOS, false));
-                        int period = receivingIntent.getIntExtra("period",
-                                mPrefs.getInt(SettingsActivity.KEY_STROBE_FREQ, 5));
+            Intent i = new Intent(context, TorchService.class);
+            if (isTorchServiceRunning(context)) {
+                context.stopServiceAsUser(i, UserHandle.CURRENT);
+            } else {
+                i.putExtra("bright", bright);
+                i.putExtra("strobe", strobe);
+                i.putExtra("period", period);
+                i.putExtra("sos", sos);
+                i.putExtra("activity", false);
+                context.startServiceAsUser(i, UserHandle.CURRENT);
+            }
+        } else if (receivingIntent.getAction().equals(START_FLASHLIGHT)) {
+            // bright setting can come from intent or from prefs depending on
+            // on what send the broadcast
+            boolean bright = receivingIntent.getBooleanExtra("bright",
+                    mPrefs.getBoolean(SettingsActivity.KEY_BRIGHT, false));
+            boolean strobe = receivingIntent.getBooleanExtra("strobe",
+                    mPrefs.getBoolean(SettingsActivity.KEY_STROBE, false));
+            boolean sos = receivingIntent.getBooleanExtra("sos",
+                    mPrefs.getBoolean(SettingsActivity.KEY_SOS, false));
+            int period = receivingIntent.getIntExtra("period",
+                    mPrefs.getInt(SettingsActivity.KEY_STROBE_FREQ, 5));
 
-                        Intent i = new Intent(context, TorchService.class);
-                        if (this.TorchServiceRunning(context)) {
-                                context.stopService(i);
-                        } else {
-                                i.putExtra("bright", bright);
-                                i.putExtra("strobe", strobe);
-                                i.putExtra("period", period);
-                                i.putExtra("sos", sos);
-
-                                context.startService(i);
-                        }
-                }
+            Intent i = new Intent(context, TorchService.class);
+            if (!isTorchServiceRunning(context)) {
+                i.putExtra("bright", bright);
+                i.putExtra("strobe", strobe);
+                i.putExtra("period", period);
+                i.putExtra("sos", sos);
+                i.putExtra("activity", true);
+                context.startServiceAsUser(i, UserHandle.CURRENT);
+            }
         }
+    }
 
-        private boolean TorchServiceRunning(Context context) {
-                ActivityManager am = (ActivityManager) context.getSystemService(Activity.ACTIVITY_SERVICE);
+    public static boolean isTorchServiceRunning(Context context) {
+        ActivityManager am = (ActivityManager) context
+                .getSystemService(Activity.ACTIVITY_SERVICE);
 
-                List<ActivityManager.RunningServiceInfo> svcList = am.getRunningServices(100);
+        List<ActivityManager.RunningServiceInfo> svcList = am
+                .getRunningServices(100);
 
-                if (!(svcList.size() > 0))
-                        return false;
-                for (RunningServiceInfo serviceInfo : svcList) {
-                        ComponentName serviceName = serviceInfo.service;
-                        if (serviceName.getClassName().endsWith(".TorchService")
-                                        || serviceName.getClassName().endsWith(".RootTorchService"))
-                                return true;
-                }
-                return false;
+        if (!(svcList.size() > 0)) {
+            return false;
         }
+        for (RunningServiceInfo serviceInfo : svcList) {
+            ComponentName serviceName = serviceInfo.service;
+            if (serviceName.getClassName().endsWith(".TorchService")
+                    || serviceName.getClassName().endsWith(".RootTorchService")) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
